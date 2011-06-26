7451e0adf21d87338cd46074d5fc1cab0ee9ca1f
msokk <mihkelsokk@gmail.com>
2011-06-26 19:17:41 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrCalc::Application.config.secret_token = 'd0f3f85ba672c5472beed62a4f5c92df9b63da20a18d737ac16e1fd27f4ca41fd776ad29ec6f16a7331d18ba4e5708a41bc4f871178df5e34c2e03a1659463a1'
